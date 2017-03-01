87848f019913ca6cdcb9ea33524cd6be6b9cdd15
Horoshi <gersonestuardo34@gmail.com>
2017-03-01 12:17:55 -0600

    Update script.js

--- Tareas de progra/HTML-master/script.js
+++ Tareas de progra/HTML-master/script.js
@@ -1,12 +1,12 @@
-var  armando =  angular.module ("armandoE",[]);
-  armando.controller("Suma",function($scope){
+var  gerson =  angular.module ("gerson",[]);
+  gerson.controller("Suma",function($scope){
     $scope.cantidad1 = 2;
     $scope.cantidad2 = 1;
     $scope.Suma = function(cantidad){
       $scope.cantidad1 + $scope.cantidad2
     };
   })
-  armando.controller("Miltiplicacion",function($scope){
+  gerson.controller("Miltiplicacion",function($scope){
     $scope.cantidad1 = 2;
     $scope.cantidad2 = 1;
     $scope.Miltiplicacion = function(cantidad){
